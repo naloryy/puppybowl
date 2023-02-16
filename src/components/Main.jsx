50a6bb2231efb7a2e1c3e9b18309452569cdc10b
Cesar bacaro-ley <cesarbacaro-ley@Cesars-MacBook-Pro.local>
2023-02-16 12:23:16 -0600

    working still o the fetch data

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar } from "./";
+import { Puppies } from "./";
 
 const Main = () => {
-  return <div id="main">{<Navbar />}</div>;
+  const [allPuppies, setallPuppies] = useState([]);
+
+  async function getPuppies() {
+    try {
+      const allPuppies = await fetch(
+        "https://fsa-puppy-bowl.herokuapp.com/api/2301-FTB-ET-WEB-FT/players"
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return (
+    <div id="main">
+      {<Navbar />}
+      <Puppies />
+    </div>
+  );
 };
 
 export default Main;
