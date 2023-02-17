e4467e28af85198544f0d11ab16e69e0f45f4ec2
Cesar bacaro-ley <cesarbacaro-ley@Cesars-MacBook-Pro.local>
2023-02-17 13:44:53 -0600

    commit after lunch

--- src/components/Puppies.jsx
+++ src/components/Puppies.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Puppies = (props) => {
   const allPuppies = props.allPuppies;
-  console.log(allPuppies);
+
   return (
     <div className="container">
-      {allPuppies.map((puppie, idx) => {
+      {allPuppies.map((puppy, idx) => {
         return (
           <div key={idx} id="cardContainer" className="pups">
-            <h3 id="name">{puppie.name}</h3>
-            <img className="pupsImg" src={puppie.imageUrl} alt="" />
+            <h3 id="name">{puppy.name}</h3>
+            <img className="pupsImg" src={puppy.imageUrl} alt="" />
             <button className="button">details</button>
           </div>
         );
