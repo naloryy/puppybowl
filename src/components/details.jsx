6eab777053e47fe412156e46eb87990826107948
Cesar bacaro-ley <cesarbacaro-ley@Cesars-MacBook-Pro.local>
2023-02-17 14:58:02 -0600

    commit befor start working on the details cards

--- src/components/details.jsx
+++ src/components/details.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { useParams } from "react-router-dom";
 
-export const details = () => {
-  return <div>details</div>;
-};
+function Details() {
+  return <div>D</div>;
+}
+
+export default Details;
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Main } from "./Main";
 export { default as Navbar } from "./Navbar";
 export { default as Puppies } from "./Puppies";
+export { default as Details } from "./Details";
